9af0d414902195a78eb93a05601ff3c165d62e45
Arkadiusz Kucharski <kucharski.ar@gmail.com>
2024-09-24 21:43:36 +0200

    zadanie domowe dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,64 +1,18 @@
-﻿using System.Formats.Asn1;
+﻿string name = "Ewa";
+char sex = 'f';
+int age = 30;
 
-var myAge = 100;
-int newAge = myAge + 5;
-
-//liczby całkowite
-
-int maxInt = int.MaxValue;
-Console.WriteLine(maxInt);
-
-int minInt = int.MinValue;
-Console.WriteLine(minInt);
-
-
-uint minUint = uint.MinValue;
-uint maxUint = uint.MaxValue;
-
-//liczby zmiennoprzecinkowe
-
-float minFloat = float.MinValue;
-float maxFloat = float.MaxValue;
-
-double minDouble = double.MinValue;
-double maxDouble = double.MaxValue;
-
-decimal minDecimal = decimal.MinValue;
-decimal maxDecimal = decimal.MaxValue;
-
-Console.WriteLine(minUint);
-Console.WriteLine(maxUint);
-Console.WriteLine(minFloat);
-Console.WriteLine(maxFloat);
-Console.WriteLine(minDouble);
-Console.WriteLine(maxDouble);
-Console.WriteLine(minDecimal);
-Console.WriteLine(maxDecimal);
-
-//zmienne tekstowe
-
-string name = "imie";
-string surname = "nazwisko";
-string fullName = name + surname + myAge;
-
-Console.WriteLine($"{name} {surname}");
-
-var cutName = name.ToArray();
-Console.WriteLine(cutName);
-
-var isValid = 5 > 6;
-Console.WriteLine(isValid);
-
-char symbol = 'a';
-
-var number1 = 10;
-var number2 = 20;
-
-if (number1 < number2)
+if (sex == 'f' && age < 30)
 {
-    Console.WriteLine("TAK");
+    Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else
+
+if (name == "Ewa" && age == 30)
 {
-    Console.WriteLine("NIE");
+    Console.WriteLine("Ewa, lat 30");
+}
+
+if (sex == 'm' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
 }
